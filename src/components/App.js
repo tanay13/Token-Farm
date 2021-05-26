7a2246822eb7fef270c2a030fab144c56b69d272
Tanay Raj <tanay.raj76@gmail.com>
2021-05-26 20:30:46 +0530

    minor fix

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,23 @@ import './App.css';
 import Web3 from 'web3';
 
 class App extends Component {
-  async componentWillUnmount() {
+  async componentWillMount() {
     await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
-  async loadBlockchainData()
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+  }
 
   async loadWeb3() {
-    if (window.ehtereum) {
-      window.web3 = new Web3(window.ehtereum);
-      await window.ehtereum.enable();
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
     } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currebtProvider);
+      window.web3 = new Web3(window.web3.currentProvider);
     } else {
       window.alert(
         'Non-Ethereum browser detected. You should consider trying Metamask'
